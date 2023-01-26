2a4532c74a9205866529298dd50ba0ddc722169e
crazycart143 <106851125+crazycart143@users.noreply.github.com>
2023-01-26 22:38:26 +0800

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import SearchIcon from "@mui/icons-material/Search";
-import IconButton from "@mui/material/IconButton";
 import { Input } from "@mui/material";
 
 function Header() {
