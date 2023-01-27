385f380241dc64577b0b3c88384ba49ac0a92ca7
crazycart143 <106851125+crazycart143@users.noreply.github.com>
2023-01-27 23:19:07 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,7 +19,6 @@ function App() {
   }, []);
 
   const [movies, setMovies] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
   useEffect(() => {
     document.title = "Home";
   }, []);
