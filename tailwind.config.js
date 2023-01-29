73bf0750a8fecdcf2cb49e0d141334a1b2a6e0e4
crazycart143 <106851125+crazycart143@users.noreply.github.com>
2023-01-29 14:03:23 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  important: "#root",
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      visibility: ["group-hover"],
+    },
+    gridTemplateRows: {
+      new3: "repeat(3, minmax(100px, 200px))", //width, height
+    },
+    gridTemplateColumns: {
+      nav1: "repeat(1, minmax(0, 0.25fr) 1fr)",
+      login1: "repeat(1, minmax(0, 1fr) 0.5fr)",
+    },
+  },
+  plugins: [require("tailwind-scrollbar")],
+};
