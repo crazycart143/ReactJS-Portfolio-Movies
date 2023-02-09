71c4b1122109b9980818d1681ca96871a8ae03b5
Keanu John Lariosa <106851125+crazycart143@users.noreply.github.com>
2023-02-09 20:02:27 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import Dashboard from "./components/Dashboard";
 import { useEffect, useState } from "react";
 import Header from "./components/Header";
